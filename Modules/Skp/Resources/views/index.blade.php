3fbf2ee0163a1061d723de56d96728bc245fd8db
widura26 <widurategalrejo@gmail.com>
2025-04-17 14:22:14 +0700

    delete module

--- Modules/Skp/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('skp::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('skp.name') !!}
-    </p>
-@endsection
