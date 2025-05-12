f4a80dc9d3aad7ca131fcf645b447d0f430503f5
widura26 <widurategalrejo@gmail.com>
2025-05-12 09:59:09 +0700

    prepare for 'Perilaku kerja' feature

---
+++ Modules/Penilaian/Entities/PerilakuKerja.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Penilaian\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PerilakuKerja extends Model
+{
+    use HasFactory;
+}
