82af299c76a9455f988ae80c04f8b158aa284221
widura26 <widurategalrejo@gmail.com>
2025-05-14 07:40:45 +0700

    feat(rencana kerja): add 'tim_kerja_id' column to rencana_kerja table

--- Modules/Penilaian/Database/Migrations/2025_04_19_125717_create_skp_rencana_kerja_table.php
+++ Modules/Penilaian/Database/Migrations/2025_04_19_125717_create_skp_rencana_kerja_table.php
@@ -17,6 +17,7 @@ class CreateSkpRencanaKerjaTable extends Migration
         Schema::dropIfExists('skp_rencana_kerja');
         Schema::create('skp_rencana_kerja', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('tim_kerja_id')->nullable();
             $table->enum('status_persetujuan', ['Belum Buat SKP', 'Belum Ajukan SKP', 'Belum Disetujui', 'Sudah Disetujui'])->default('Belum Buat SKP');
             $table->enum('status_realisasi', ['Belum Diajukan', 'Sudah Diajukan', 'Belum Dievaluasi', 'Sudah Dievaluasi'])->default('Belum Diajukan');
             $table->string('rating_hasil_kerja')->nullable();
@@ -24,11 +25,13 @@ class CreateSkpRencanaKerjaTable extends Migration
             $table->string('rating_perilaku')->nullable();
             $table->text('deskripsi_rating_perilaku')->nullable();
             $table->enum('predikat_akhir', ['Istimewa', 'Baik', 'Cukup', 'Kurang', 'Sangat Kurang', 'Butuh Perbaikan'])->nullable();
-            $table->foreignId('periode_id')->nullable();
+            $table->foreignId('periode_id')->nullable()->constrained('periodes')->onDelete('cascade');
             $table->unsignedBigInteger('pegawai_id')->nullable();
-            $table->foreign('pegawai_id')->references('id')->on('pegawai')->onDelete('cascade');
             $table->unsignedBigInteger('lampiran_id')->nullable();
             $table->timestamps();
+
+            $table->foreign('pegawai_id')->references('id')->on('pegawai')->onDelete('cascade');
+            $table->foreign('tim_kerja_id')->references('id')->on('tim_kerja')->onDelete('cascade');
         });
     }
 
